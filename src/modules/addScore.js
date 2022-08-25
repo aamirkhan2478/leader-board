bcd1e7a43f6ebb1415973f13bd6f895e6f8ffe0e
Aamir Khan <aamir.khan2478@gmail.com>
2022-08-25 22:22:27 +0500

    Add some styling and add validations.

--- src/modules/addScore.js
+++ src/modules/addScore.js
@@ -1,4 +1,4 @@
-import { message, nameField, scoreField } from "./getElements.js";
+import { error, success, nameField, scoreField,baseUrl } from "./getElements.js";
 
 class AddScore {
   addScore = async (e) => {
@@ -6,30 +6,35 @@ class AddScore {
     const nameText = nameField.value;
     const scoreText = scoreField.value;
     try {
-      const sendData = await fetch(
-        "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/RVLHmrTjFYooSfq9ZHuB/scores/",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            user: nameText,
-            score: parseInt(scoreText, 10),
-          }),
+      const sendData = await fetch(baseUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-      );
-      const { result } = await sendData.json();
-      message.innerText = result;
+        body: JSON.stringify({
+          user: nameText,
+          score: parseInt(scoreText, 10),
+        }),
+      });
+      const { result, message } = await sendData.json();
+      if (result) {
+        success.innerText = result;
+        success.className = "alert alert-success";
+        setInterval(() => {
+          success.className = "d-none";
+        }, 3000);
+      } else {
+        error.innerText = message;
+        error.className = "alert alert-danger";
+        setInterval(() => {
+          error.className = "d-none";
+        }, 3000);
+      }
       nameField.value = "";
       scoreField.value = "";
     } catch (error) {
       console.error(error.message);
     }
-
-    setInterval(() => {
-      message.style.display = "none";
-    }, 2500);
   };
 }
 
--- src/modules/getElements.js
+++ src/modules/getElements.js
@@ -1,6 +1,9 @@
-export const saveScore = document.getElementById('saveScore');
-export const nameField = document.getElementById('nameField');
-export const scoreField = document.getElementById('scoreField');
-export const results = document.getElementById('results');
-export const refreshData = document.getElementById('refresh');
-export const message = document.getElementById('msg');
+export const saveScore = document.getElementById("saveScore");
+export const nameField = document.getElementById("nameField");
+export const scoreField = document.getElementById("scoreField");
+export const results = document.getElementById("results");
+export const refreshData = document.getElementById("refresh");
+export const error = document.getElementById("error");
+export const success = document.getElementById("success");
+export const baseUrl =
+  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/RVLHmrTjFYooSfq9ZHuB/scores/";
